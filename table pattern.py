07ad2028251e0222d8995b779829ba65640eceb8
Mahalakshmibonasi <106964851+Mahalakshmibonasi@users.noreply.github.com>
2022-12-17 11:47:52 +0530

    Add files via upload

---
+++ table pattern.py
@@ -0,0 +1,28 @@
+# i=1
+# while i<=100:
+#     j=1
+#     while j<=10:
+#         print(i,"*",j,"=",i*j)
+#         j=j+1
+#     print()
+#     i=i+1
+# i=0 
+# while i<=10:
+#     i=i+1
+#     if i==5:
+#         pass
+#     if i==6:
+#         continue
+#     if i==9:
+#         break
+#     print(i)
+
+# a=int(input("enter the num"))
+# i=0
+# b=[]
+# while i<a:
+#     c=int(input("enter the num"))
+#     b.append(a)
+#     i=i+1
+# print(b)
+
